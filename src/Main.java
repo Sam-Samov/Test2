c918716ef3c3a54ad74fdce60b27283361624a1d
Sam-Samov <waxgem@gmail.com>
2024-10-08 01:05:22 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,38 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.US);
         while (true) {
-        System.out.println("Введите первое число:");
-        double x = sc.nextDouble();
-        System.out.println("Введите второе число:");
-        double y = sc.nextDouble();
-        System.out.println("Выберите операцию (+, -, *, /)");
-        char operation = sc.next().charAt(0);
-        String oper = "";
-        double res = 0;
-        switch (operation) {
-            case '+':
-                res = Calculator.sumUp(x,y);
-                oper = "Операция сложения: ";
-                break;
-            case '-':
-                res = Calculator.subtract(x,y);
-                oper = "Операция вычитания: ";
-                break;
-            case '*':
-                res = Calculator.multiply(x,y);
-                oper = "Операция умножения: ";
-                break;
-            case '/':
-                res = Calculator.division(x,y);
-                oper = "Операция деления: ";
-                break;
-        }
-        ResultWriterService.printResult(oper, x, y, operation, res);
+            System.out.println("Введите первое число:");
+            double x = sc.nextDouble();
+            System.out.println("Введите второе число:");
+            double y = sc.nextDouble();
+            System.out.println("Выберите операцию (+, -, *, /)");
+            char operation = sc.next().charAt(0);
+            String oper = "";
+            double res = 0;
+            switch (operation) {
+                case '+':
+                    res = Calculator.sumUp(x, y);
+                    oper = "Операция сложения: ";
+                    ResultWriterService.printResult(oper, x, y, operation, res);
+                    break;
+                case '-':
+                    res = Calculator.subtract(x, y);
+                    oper = "Операция вычитания: ";
+                    ResultWriterService.printResult(oper, x, y, operation, res);
+                    break;
+                case '*':
+                    res = Calculator.multiply(x, y);
+                    oper = "Операция умножения: ";
+                    ResultWriterService.printResult(oper, x, y, operation, res);
+                    break;
+                case '/':
+                    res = Calculator.division(x, y);
+                    oper = "Операция деления: ";
+                    ResultWriterService.printResult(oper, x, y, operation, res);
+                    break;
+                default:System.out.println("Неверная операция!");
+
+            }
         }
     }
 }
